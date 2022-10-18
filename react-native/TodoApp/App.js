bdbdb45ad5feeea06311e17ed7fffbfb8178d53f
JB <jungbeomsu@naver.com>
2022-10-18 23:42:06 +0900

    style: Layout 세팅

---
+++ react-native/TodoApp/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
+import AddTodo from './components/AddTodo';
+import DateHead from './components/DateHead';
+import Empty from './components/Empty';
+
+function App() {
+  const today = new Date();
+
+  return (
+    <SafeAreaView style={styles.block}>
+      <DateHead date={today} />
+      <Empty />
+      <AddTodo />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+  },
+});
+
+export default App;
